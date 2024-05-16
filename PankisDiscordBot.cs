4dce8d23d10487d7452669da42bcbb84b6a72a21
Kesomannen <bobbo.nidor@gmail.com>
2024-05-16 11:14:03 +0200

    change intents

--- PankisDiscordBot.cs
+++ PankisDiscordBot.cs
@@ -258,7 +258,7 @@ public class PankisDiscordBot {
 
     public static async Task<PankisDiscordBot> Create(string token, TextToSpeech tts, Chat chat,  MessageFilter messageFilter) {
         var config = new DiscordSocketConfig {
-            GatewayIntents = GatewayIntents.All
+            GatewayIntents = GatewayIntents.GuildMembers | GatewayIntents.GuildMessages | GatewayIntents.MessageContent
         };
         
         var client = new DiscordSocketClient(config);
